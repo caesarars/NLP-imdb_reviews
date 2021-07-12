98ea156abcdde8eea55254ddbef0a71724bf8b35
Caesar Arssetya <38849354+caesarars@users.noreply.github.com>
2021-07-12 10:23:15 +0700

    Update script.py

--- script.py
+++ script.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 import tensorflow_datasets as tfds
 
-imdb = tfds.load("imdb_reviews", with_info=True, as_supervised=True)
+imdb , info= tfds.load("imdb_reviews", with_info=True, as_supervised=True)
 
 train_data, test_data = imdb['train'], imdb['test']
